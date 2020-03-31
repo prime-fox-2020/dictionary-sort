9a6bdd72f367745aeeba0fb27bb0a0155e510def
rezarr-dev <rezarr.business@gmail.com>
2020-03-31 20:29:52 +0700

    dictionary sort done

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 function dictionarySort(dictionaries) {
   // your code below here...
-
+  for (let i = 0; i < dictionaries.length; i++){
+    for (let j = 0; j < dictionaries.length; j++){
+      if (dictionaries[i] < dictionaries[j]){
+        let temp = dictionaries[i]
+        dictionaries[i] = dictionaries[j]
+        dictionaries[j] = temp
+      }
+    }
+  }
+  return dictionaries
 }
 
 module.exports = dictionarySort
