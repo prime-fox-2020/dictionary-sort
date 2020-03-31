63e8acbf3b154371a6a99ba81eaa23f910f0bdaf
fahrulnoer <fahrulnoer17@gmail.com>
2020-03-31 17:03:48 +0700

    dictionary-sort

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 function dictionarySort(dictionaries) {
   // your code below here...
-
+  for (let i = 0; i < dictionaries.length; i++) {
+    for (let j = i + 1; j < dictionaries.length; j++) {
+      if(dictionaries[i] > dictionaries[j]){
+        let temp = dictionaries[i]
+        dictionaries[i] = dictionaries[j]
+        dictionaries[j] = temp
+      }
+    }
+  }
+  return dictionaries
 }
+
+var arrOfWord1 = [ 'makan', 'duduk', 'tidur', 'terbang' ]
+var arrOfWord2 = [ 'anggi', 'angga', 'ani', 'adi' ]
+
+console.log(dictionarySort(arrOfWord1))
+// [ 'duduk', 'makan', 'terbang', 'tidur' ]
+
+console.log(dictionarySort(arrOfWord2))
+// [ 'adi', 'angga', 'anggi', 'ani' ]
 
 module.exports = dictionarySort
