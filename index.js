2130b1e9e240179677dcf6cddb1c704ec10f8ce7
Eko Prasetyo <ekoprasetyo.me@gmail.com>
2020-03-31 22:28:22 +0700

    Release 0 done

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 function dictionarySort(dictionaries) {
-  // your code below here...
-
+  for (let i = 0; i < dictionaries.length; i++) {
+    let q, min = i;
+    for (let j = i; j < dictionaries.length; j++) {
+      if (dictionaries[j] < dictionaries[min]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      q = dictionaries[i];
+      dictionaries[i] = dictionaries[min];
+      dictionaries[min] = q;
+    }
+  }
+  return dictionaries;
 }
+console.log(dictionarySort(['makan', 'duduk', 'tidur', 'terbang']));
+console.log(dictionarySort(['anggi', 'angga', 'ani', 'adi']));
 
 module.exports = dictionarySort
