7039c5f554b33fd8128ff50d13f4a4da36568b27
michaelm96 <michaelarceus96@gmail.com>
2020-03-31 15:48:34 +0700

    sorting based on alphabet

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 function dictionarySort(dictionaries) {
   // your code below here...
-
+  for (let i = 0; i < dictionaries.length; i++) {
+    for (let j = 0; j < dictionaries.length -1; j++) {
+      if(dictionaries[j] > dictionaries[j+1]){
+        let temp = dictionaries[j]
+        dictionaries[j] = dictionaries[j+1]
+        dictionaries[j+1] = temp
+      }
+    }
+  }
+  return dictionaries
 }
+
+const test_word_1 = ["makan", "duduk", "tidur", "terbang"];
+const test_word_2 = ["anggi", "angga", "ani", "adi"];
+
+console.log(dictionarySort(test_word_1))
+console.log(dictionarySort(test_word_2))
 
 module.exports = dictionarySort
