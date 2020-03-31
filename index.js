d4f9f9b13de2e094731943d215d275ee47d8db3f
Muhammad Risky Nugraha <muhammadrisky83@gmail.com>
2020-03-31 11:22:03 +0700

    done

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 function dictionarySort(dictionaries) {
-  // your code below here...
+  for (let i = 0; i < dictionaries.length; i++) {
+    for (let j = i + 1; j < dictionaries.length; j++) {
+      if (dictionaries[j] < dictionaries[i]) {
+        let swap = dictionaries[j];
+        dictionaries[j] = dictionaries[i];
+        dictionaries[i] = swap;
+      }
+    }
+  }
 
+  var result = '';
+  for (let k = 0; k < dictionaries.length; k++) {
+    if (k === dictionaries.length - 1) {
+      result += dictionaries[k];
+    } else {
+      result += dictionaries[k] + ',';
+    }
+  }
+  return result;
 }
 
-module.exports = dictionarySort
+const dictionary1 = ['makan', 'minum', 'lari', 'ngoding', 'tidur', 'belajar'];
+const dictionary2 = ['anggi', 'angga', 'ani', 'adi']
+
+console.log(dictionarySort(dictionary1));
+console.log(dictionarySort(dictionary2));
+
+module.exports = dictionarySort;
