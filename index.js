5d63681fff6e99da72ae43122ddde9fb790bcab2
issdq <ismail.ashshidiq@gmail.com>
2020-03-31 21:37:56 +0700

    done pake bubble sort

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function dictionarySort(dictionaries) {
   // your code below here...
+  for (let i = 0; i < dictionaries.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (dictionaries[i] < dictionaries[j]) {
+        let temp = dictionaries[i]
+        dictionaries[i] = dictionaries[j]
+        dictionaries[j] = temp
+      }
+    }
+  }
 
+  return dictionaries.join(', ')
 }
+
+console.log(dictionarySort(['makan', 'duduk', 'tidur', 'terbang']));
+console.log(dictionarySort(['anggi', 'angga', 'ani', 'adi']));
 
 module.exports = dictionarySort
