81e2c439b4ec84bb80b3423dd45687610ad8c195
zuhdi <zudhy.duddy@gmail.com>
2020-03-31 16:17:44 +0700

    done dictionary sort

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 function dictionarySort(dictionaries) {
   // your code below here...
-
+  for (let i = 0; i < dictionaries.length; i++) {
+    for (let j = i + 1; j < dictionaries.length; j++) {
+      if (dictionaries[i] > dictionaries[j]){
+        var swap = dictionaries[i]
+        dictionaries[i] = dictionaries[j]
+        dictionaries[j] = swap
+      }
+    }
+  }
+  return dictionaries
 }
 
+var arrOfWords1 = ['makan', 'duduk', 'tidur', 'terbang']
+var arrOfWords2 = ['anggi', 'angga', 'ani', 'adi']
+
+console.log(dictionarySort(arrOfWords1))
+console.log(dictionarySort(arrOfWords2))
+
 module.exports = dictionarySort
+
+
