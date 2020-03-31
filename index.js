34d79f77acfa9d80a24ed1d98096947e33961f03
fadhlanfm <fafadhlanriz@gmail.com>
2020-03-31 23:32:59 +0700

    Release 0: OK, using bubble sort

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 function dictionarySort(dictionaries) {
   // your code below here...
+  for(let i = 0; i < dictionaries.length; i++){
+    for(let j = 0; j < dictionaries.length - i - 1; j++){
+      if(dictionaries[j] > dictionaries[j+1]){
+        let tmp = dictionaries[j]
+        dictionaries[j] = dictionaries[j+1]
+        dictionaries[j+1] = tmp
+      }
+    }
+  }
 
+  let output = ''
+
+  for(let i = 0; i < dictionaries.length; i++){
+    if(i === dictionaries.length - 1){
+      output += dictionaries[i]
+    }else{
+      output += dictionaries[i] + ','
+    }
+  }
+  return output
 }
+
+console.log(dictionarySort(['makan','duduk','tidur','terbang']))
+console.log(dictionarySort(['anggi','angga','ani','adi']))
 
 module.exports = dictionarySort
