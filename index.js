3ab4c8ef245c620b553ef0f99fc0e4361c2428f9
elvanrafif <terlanjurbuta@gmail.com>
2020-03-31 20:00:29 +0700

    add tugas dictionary sort

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 function dictionarySort(dictionaries) {
-  // your code below here...
+  var temp = '';
+
+  for (var i=dictionaries.length-1; i>=0; i--) {
+    for (var j=0; j<dictionaries.length;j++) {
+      if (dictionaries[j]>dictionaries[j+1]) {
+        temp = dictionaries[j];
+        dictionaries[j] = dictionaries[j+1];
+        dictionaries[j+1] = temp;
+      }
+    }
+  }
+
+  var hasil = '';
+
+  for (var i=0; i<dictionaries.length; i++) {
+    if (i!==dictionaries.length-1) {
+      hasil+=dictionaries[i] + ', ';
+    } else {
+      hasil+=dictionaries[i];
+    }
+  }
+
+  return hasil;
 
 }
+
+// console.log(dictionarySort(['makan','duduk','tidur','terbang']));
+console.log(dictionarySort(['anggi','angga','ani','adi']));
+
 
 module.exports = dictionarySort
