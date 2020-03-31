f30ed187da1b1f422dc4628e2785d015b018835f
pippop21 <afifar38@gmail.com>
2020-03-31 15:50:35 +0700

    update index.js and add testcase from index.test.js to index.js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function dictionarySort(dictionaries) {
   // your code below here...
-
+  for(let i = 0; i < dictionaries.length; i++){
+    for(let j = 0; j < dictionaries.length-1; j++){
+      if(dictionaries[j] > dictionaries[j+1]){
+        [dictionaries[j], dictionaries[j+1]] = [dictionaries[j+1], dictionaries[j]];
+      }
+    }
+  }
+  return dictionaries.join(", ");
 }
+
+const test_word_1 = ["makan", "duduk", "tidur", "terbang"];
+const test_word_2 = ["anggi", "angga", "ani", "adi"];
+
+console.log(dictionarySort(test_word_1));
+console.log(dictionarySort(test_word_2));
 
 module.exports = dictionarySort
