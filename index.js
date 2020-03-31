d1992ef82a3094f6d3a0b38008289e57f11e96a0
riolly <albert.r.cahyantara@gmail.com>
2020-03-31 21:42:36 +0700

    Dictionary short using buble sort algoritm

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function dictionarySort(dictionaries) {
-  // your code below here...
-
+  // Bubble short
+  for (let i = 0; i < dictionaries.length - 1; i++) {
+    for (let j = 0; j < dictionaries.length - 1 - i; j++) {
+      if (dictionaries[j] > dictionaries[j + 1]) {
+        debugger;
+        let temp = dictionaries[j];
+        dictionaries[j] = dictionaries[j + 1];
+        dictionaries[j + 1] = temp;
+      }
+    }
+  }
+  return dictionaries;
 }
-
+console.log(dictionarySort(['makan', 'duduk', 'tidur', 'terbang']));
+console.log(dictionarySort(['anggi', 'angga', 'ani', 'adi']));
 module.exports = dictionarySort
