f2bb9589b3218ba113bce52dfafddd722784d918
Mercen1 <nurhabib102@gmail.com>
2020-03-31 21:48:01 +0700

    dictionary sort solve

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 function dictionarySort(dictionaries) {
   // your code below here...
+  var hasil ='';
+  
+  for (let i = 0; i < dictionaries.length; i++) {
+      for (let j = 0; j < dictionaries.length-1; j++) {
+          if(dictionaries[j]>dictionaries[j+1]){
+              var temp = dictionaries[j];
+              dictionaries[j]=dictionaries[j+1];
+              dictionaries[j+1]=temp
+          }
+      }
+  }
+  hasil = dictionaries.join(',')
 
+  return hasil
 }
+
+
+console.log(dictionarySort(['makan','duduk','tidur','terbang']))
+console.log(dictionarySort(['anggi','angga','ani','adi']))
+
+
 
 module.exports = dictionarySort
