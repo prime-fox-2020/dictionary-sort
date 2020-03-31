0a5d5222405c1319148f120d08521a7b4da4a071
Vincent Guizot <30890039+CenZo128@users.noreply.github.com>
2020-03-31 17:22:18 +0700

    Fixed

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 function dictionarySort(dictionaries) {
   // your code below here...
+  let swap
+  for (var i = 0; i < dictionaries.length; i++) {
+    for (var j = i; j >= 0; j--) {
+    
+      if (dictionaries[j] < dictionaries[j-1]) {
+        swap = dictionaries[j-1]
+        dictionaries[j-1] = dictionaries[j]
+        dictionaries[j] = swap
+        }    
+      }
 
+  }
+  return dictionaries
 }
+
+var arrOfWord = ['makan','duduk','tidur','terbang']
+console.log(dictionarySort(arrOfWord).join(','))
 
 module.exports = dictionarySort
