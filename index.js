3fbc141d9f35e50dca3958ba5ff6a86c35312fe0
rakdhan <rakahamijoyo@gmail.com>
2020-03-31 22:07:27 +0700

    noice_dictSort

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function dictionarySort(dictionaries) {
   // your code below here...
+  for (let i = 0; i < dictionaries.length; i++) {
+    for (let k = 0; k < dictionaries.length; k++) {
+      if (dictionaries[i] < dictionaries[k]) {
+        let stby = dictionaries[i]
+        dictionaries[i] = dictionaries[k]
+        dictionaries[k] = stby
+      }      
+    }    
+  }
+  return dictionaries
 
 }
-
+const test_word_1 = ["makan", "duduk", "tidur", "terbang"];
+const test_word_2 = ["anggi", "angga", "ani", "adi"];
+console.log(dictionarySort(test_word_1))
+console.log(dictionarySort(test_word_2))
 module.exports = dictionarySort
