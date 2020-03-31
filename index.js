6fc3bf19209605630c17ee618509d7c97abeca30
rinotoharto <rinotoharto@gmail.com>
2020-03-31 18:35:17 +0700

    dictionary sort done

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-function dictionarySort(dictionaries) {
+function dictionariesSort(dictionaries) {
   // your code below here...
-
+  for(let i = 0; i < dictionaries.length; i++) {
+    let min = i
+    for(let j = i+1; j < dictionaries.length; j++) {
+        if(dictionaries[j] < dictionaries[min]) {
+            min = j
+        }
+    }
+    let swapped = dictionaries[min]
+    dictionaries[min] = dictionaries[i]
+    dictionaries[i] = swapped
+  }
+  return dictionaries
 }
 
-module.exports = dictionarySort
+let arr = ['makan', 'duduk', 'tidur', 'terbang']
+// duduk, makan, terbang, tidur
+console.log(dictionariesSort(arr))
+
+let arr2 = ['anggi', 'angga', 'ani', 'adi']
+// adi, angga, anggi, ani
+console.log(dictionariesSort(arr2))
+
+
+
+module.exports = dictionariesSort
