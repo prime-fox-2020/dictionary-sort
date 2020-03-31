6581fc6d0be7d3196cf443aa0ad6a3ed3a26a372
andreanwirapradana <61081321+andreanwirapradana@users.noreply.github.com>
2020-03-31 18:15:15 +0700

    tugas dictionary sort

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 function dictionarySort(dictionaries) {
   // your code below here...
-
+  for (let i = 0; i < dictionaries.length; i++) {
+      let min = i;
+      for (let j = i + 1; j < dictionaries.length; j++) {
+          if (dictionaries[min] > dictionaries[j]) {
+              min = j;
+          }
+      }
+      if (min !== i) {
+          let tmp = dictionaries[i];
+          dictionaries[i] = dictionaries[min];
+          dictionaries[min] = tmp;
+      }
+  }
+  return dictionaries;
 }
 
+var arrOfWords = ['makan', 'duduk', 'tidur', 'terbang'];
+console.log(dictionarySort(arrOfWords))
 module.exports = dictionarySort
