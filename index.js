12e0084cde2e0dd0f47f9346162fb5f411e0dd7e
Zetra416 <penguin_vs_yeti@yahoo.com>
2020-03-31 20:36:06 +0700

    finished file index.js

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 function dictionarySort(dictionaries) {
-  // your code below here...
+  let temp = [];
 
+  for (let i = 0; i < dictionaries.length; i++) {
+    for(let j = 0; j < dictionaries.length; j++) {
+      if (dictionaries[j] > dictionaries[j+1]) {
+        temp = dictionaries[j];
+        dictionaries[j] = dictionaries[j+1];
+        dictionaries[j+1] = temp;
+      }
+    }
+  }
+return dictionaries.toString();
 }
 
+
+
+
+let arrOfWord = ["makan","duduk","tidur","terbang"];
+let arrOfWord2 = ["anggi","angga","ani","adi"];
+console.log(dictionarySort(arrOfWord));
+console.log(dictionarySort(arrOfWord2));
 module.exports = dictionarySort
