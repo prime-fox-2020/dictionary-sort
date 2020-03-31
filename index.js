6573e0f15475ffd5848956777b6991bab2d0791f
Adib Faqih <foezst@gmail.com>
2020-03-31 22:57:48 +0700

    dictionary-sort, solved

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function dictionarySort(dictionaries) {
-  // your code below here...
+  for (var i = 0; i < dictionaries.length; i++) {
+    var idx = i;
+    for (var j = i; j < dictionaries.length; j++) {
+      if (dictionaries[j] < dictionaries[idx]) {
+        idx = j;
+      }
+    }
+    var temp = dictionaries[i];
+    dictionaries[i] = dictionaries[idx];
+    dictionaries[idx] = temp;
+  }
 
+  return dictionaries.join();
 }
 
 module.exports = dictionarySort
