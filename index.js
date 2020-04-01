bd03547f6d14d25da226decf978bfeaa8701c815
permaficus <abukhalif2019@gmail.com>
2020-04-01 09:37:18 +0700

    Dictionary Sort Done!

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function dictionarySort(dictionaries) {
   // your code below here...
+  const sort = n => {
+    let f = 0;
+    for (let min, i=0;i<n.length; i++) {
+      if (n[i] > n[i+1]) {
+        min = n[i+1];
+        n[i+1] = n[i];
+        n[i] = min;
+        f++;
+      }
+    }
+    return f == 0 ? n:sort(n)
+  }
 
+  return sort(dictionaries).toString().split(',').join(', ');
 }
 
 module.exports = dictionarySort
